eb35b828c04c4a588fda2a49c1e6ede50404ec30
Arinirache <arinirache9890858408@gmail.com>
2025-03-25 10:31:19 +0530

    Add files via upload

---
+++ hello1.py
@@ -0,0 +1,20 @@
+
+num = int(input("enter a number"))
+
+
+factorial = 1
+
+
+for i in range(1, num + 1):
+    factorial *= i
+
+
+print(f"The factorial of {num} is {factorial}")
+
+
+
+
+name = input("What's your name? ")
+
+
+print(f"Hello, {name}! Welcome to the program.")
